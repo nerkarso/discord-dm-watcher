c9e8a304f53b34201c4247b053a0d51ab1420f60
Ner Karso <nerpkarso@gmail.com>
2025-03-08 03:19:13 -0300

    fix: routes not working in production

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,17 @@ import { routes as oauth2Routes } from './oauth2';
 
 const PORT = Bun.env.PORT || 5000;
 
+const routes: Record<string, (req: Request) => Promise<Response>> = {
+  ...oauth2Routes,
+  '/': async () => new Response('OK'),
+};
+
 Bun.serve({
-  routes: {
-    ...oauth2Routes,
-    '/': new Response('OK'),
-  },
-  fetch() {
-    return new Response('Route Not Found', { status: 404 });
+  routes,
+  fetch(req) {
+    const url = new URL(req.url);
+    const route = routes[url.pathname];
+    return route ? route(req) : new Response('Route Not Found', { status: 404 });
   },
   port: PORT,
 });
