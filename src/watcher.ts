274be2a103e1b3db00ee66df21cc118118996173
Ner Karso <nerpkarso@gmail.com>
2025-03-08 02:46:37 -0300

    feat: add rate limit to push notification

--- src/watcher.ts
+++ src/watcher.ts
@@ -1,6 +1,8 @@
 import { consola } from 'consola';
 import { Client } from 'discord.js-selfbot-v13';
 
+const sessionStore = new Map<string, any>();
+
 const client = new Client();
 
 client.on('ready', () => {
@@ -10,17 +12,29 @@ client.on('ready', () => {
 client.on('messageCreate', async (message) => {
   if (message.channel.type === 'DM' && !message.author.bot) {
     try {
-      const response = await fetch(Bun.env.NTFY_URL!, {
-        method: 'POST',
-        headers: {
-          Icon: Bun.env.NTFY_ICON || 'https://img.icons8.com/?size=192&id=M725CLW4L7wE&format=png',
-          Tags: 'speech_balloon',
-        },
-        body: `DM from ${message.author.displayName}`,
-      });
-      if (!response.ok) throw new Error('Failed to send notification');
-      const data = await response.json();
-      consola.success('Notification sent |', data);
+      const rateLimitKey = `dm_${message.author.id}`;
+      const rateLimitTime = 15 * 60 * 1000; // 15 minutes in milliseconds
+      const lastSent = sessionStore.get(rateLimitKey);
+      const currentTime = Date.now();
+
+      if (!lastSent || currentTime - lastSent >= rateLimitTime) {
+        const response = await fetch(Bun.env.NTFY_URL!, {
+          method: 'POST',
+          headers: {
+            Icon: Bun.env.NTFY_ICON || 'https://img.icons8.com/?size=192&id=M725CLW4L7wE&format=png',
+            Tags: 'speech_balloon',
+          },
+          body: `DM from ${message.author.displayName}`,
+        });
+
+        if (!response.ok) throw new Error('Failed to send notification');
+        const data = await response.json();
+        consola.success('Notification sent |', data);
+
+        sessionStore.set(rateLimitKey, currentTime);
+      } else {
+        consola.info('Rate limit applied, notification not sent');
+      }
     } catch (error: any) {
       consola.error(error.message);
     }
