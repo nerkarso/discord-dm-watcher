8e3ea74bc15fa698191c949ae98188191a4f9c8e
Ner Karso <nerpkarso@gmail.com>
2025-03-24 11:58:17 -0300

    feat: add separate config and endpoint

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+/**
+ * Holds data about the DMs
+ *
+ * @returns `("dm_<user_id>", timestamp)[]`
+ */
+export const dmStore = new Map<string, any>();
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import { consola } from 'consola';
+import { dmStore } from './config';
 import { routes as oauth2Routes } from './oauth2';
 
 const PORT = Bun.env.PORT || 5000;
@@ -6,6 +7,19 @@ const PORT = Bun.env.PORT || 5000;
 const routes: Record<string, (req: Request) => Promise<Response>> = {
   ...oauth2Routes,
   '/': async () => new Response('OK'),
+  '/config': async () => {
+    const dmStoreList: any[] = [];
+
+    dmStore.forEach((value, key) => {
+      dmStoreList.push({
+        user: key,
+        lastSent: value,
+        lastSentFormatted: new Date(value).toLocaleString(),
+      });
+    });
+
+    return Response.json({ dmStore: dmStoreList });
+  },
 };
 
 Bun.serve({
